c9e1d0d17d9c64e6b5df0f22728500fea68900ff
Msaideepak <maharthisaideepak@gmail.com>
2026-04-23 12:44:58 +0530

    classstudent,professor

---
+++ day22.py
@@ -0,0 +1,54 @@
+'''class person:
+    def __init__(self , name, age, education, id, course):
+        self.name = name
+        self.age = age
+        self.education = education
+        self.id = id
+        self.course = course
+    def display(self):
+        print("name: ",self.name)
+        print("age: ",self.age)
+        print("education: ",self.education)
+        print("id: ", self.id)
+        print("course: ", self.course)
+obj = person("deepak", "24", "btech", "21nu1a0570", "python")
+obj.display()
+
+class person:
+     def __init__(self , name, age, education, id, course, professor):
+        self.name = name
+        self.age = age
+        self.education = education
+        self.id = id
+        self.course = course
+        self.professor = professor
+     def display(self):
+        print("name: ",self.name)
+        print("age: ",self.age)
+        print("education: ",self.education)
+        print("id: ", self.id)
+        print("course: ", self.course)
+        print("professor: ", self.professor)
+obj = person("deepak", "24", "btech", "21nu1a0570", "python", "bhanuteja")
+obj.display()
+
+'''
+class person:
+     def __init__(self , name, age, education, id, course, professor, location):
+        self.name = name
+        self.age = age
+        self.education = education
+        self.id = id
+        self.course = course
+        self.professor = professor
+        self.location = location
+     def display(self):
+        print("name: ",self.name)
+        print("age: ",self.age)
+        print("education: ",self.education)
+        print("id: ", self.id)
+        print("course: ", self.course)
+        print("professor: ", self.professor)
+        print("location: ", self.location)
+obj = person("deepak", "24", "btech", "21nu1a0570", "python", "bhanuteja", "pendurthi")
+obj.display()
